b26e39d8aef8a43f2b8462e3ef5fde3653afa181
tstubb <Trevor.Stubblefield1995@gmail.com>
2017-04-26 17:06:46 -0600

    final run.sh changes

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ export PYTHONHASHSEED=0
 export SPARK_YARN_USER_ENV=PYTHONHASHSEED=0
 
 hadoop fs -rmr /spark-out
-/usr/local/spark/bin/spark-submit --master yarn --driver-memory 3g train.py
+/usr/local/spark/bin/spark-submit --master yarn --driver-memory 3g --executor-cores 3 --num-executors 20 train.py
 hadoop fs -cat /spark-out/part-00000 > out.txt
 
 #rm -r out
